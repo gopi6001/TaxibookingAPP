0b18a8965c30d465072fd8ded71eab6b70e99d4d
gopi6001 <58928707+gopi6001@users.noreply.github.com>
2019-12-16 12:09:13 +0530

    Add files via upload

---
+++ trail.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project
+{
+    public class trail
+    {
+        public string SourceAddress { get; set; }
+        public string DestinationAddress { get; set; }
+    }
+}
